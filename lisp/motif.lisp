ae415fbc8c5f35b421ae22db844ef94e00a238bf
Break Yang <BreakDS@gmail.com>
2014-11-18 08:32:19 -0500

    init motif

--- lisp/motif.lisp
+++ lisp/motif.lisp
@@ -2,6 +2,44 @@
 ;;;; Author: Break Yang <breakds@gmail.com>
 
 (defun motif-enumeration (dna-list pattern-length tolerance)
-  (let ((tested (make-hash-table :test #'equal))
+  (let ((template (coerce (loop for i below pattern-length collect #\T)
+                          'string)))
+    ;; This gen-neighbors call generates all the possible pattern with
+    ;; length pattern-length.
+    (loop for candidate in (gen-neighbors template pattern-length)
+       when (every #`,(not (null (approximate-pattern-match x1
+                                                            candidate
+                                                            tolerance)))
+                   dna-list)
+       collect candidate)))
+
+(defun best-fit-distance (dna pattern)
+  (loop for i from 0 to (- (length dna) (length pattern))
+     minimize (hamming-distance pattern
+                                (subseq dna 
+                                        i (+ i (length pattern))))))
+
+(defun best-fit-score (dna-list pattern)
+  (apply #'+ (mapcar #`,(best-fit-distance x1 pattern)
+                     dna-list)))
+
+(defun median-string (dna-list pattern-length)
+  (let ((template (coerce (loop for i below pattern-length collect #\T)
+                          'string))
+        (best (list (1+ pattern-length) "")))
+    ;; This gen-neighbors call generates all the possible pattern with
+    ;; length pattern-length.
+    (loop 
+       for candidate in (gen-neighbors template pattern-length)
+       for score = (best-fit-score dna-list candidate)
+       when (< score (car best))
+       do (setf best (list score candidate)))
+    (cadr best)))
+         
+  
+  
+                                  
+
+        
         
   
